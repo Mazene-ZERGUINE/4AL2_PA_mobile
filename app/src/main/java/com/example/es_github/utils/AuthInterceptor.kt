57376148e64d9dc562f33552c408c8a45d703db4
Mazene <mmazenezerguine@gmail.com>
2024-06-01 22:26:09 +0200

    interceptor

---
+++ app/src/main/java/com/example/es_github/utils/AuthInterceptor.kt
@@ -0,0 +1,18 @@
+package com.example.es_github.utils
+
+import android.content.Context
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val context: Context): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+        val token = SecureStorage.getToken(context)
+
+        token?.let {
+            requestBuilder.addHeader("Authorization", "Bearer $it")
+        }
+
+        return chain.proceed(requestBuilder.build())
+    }
+}
