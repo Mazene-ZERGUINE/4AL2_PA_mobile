202fea2b8c1d029f94d8c8868370d3d35d832579
Mazene <mmazenezerguine@gmail.com>
2024-06-01 16:31:23 +0200

    init project

---
+++ app/src/main/java/com/example/es_github/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.es_github
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.es_github.ui.theme.Es_githubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Es_githubTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Es_githubTheme {
+        Greeting("Android")
+    }
+}
