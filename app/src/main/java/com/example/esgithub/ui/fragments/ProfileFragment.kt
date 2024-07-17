78f517cbc0dffc7b17fc697c1362a73e9e5be99d
Mazene <mmazenezerguine@gmail.com>
2024-07-18 00:55:51 +0200

    profile Co-authored-by: LoicZHU <57401386+LoicZHU@users.noreply.github.com>

--- app/src/main/java/com/example/esgithub/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/esgithub/ui/fragments/ProfileFragment.kt
@@ -1,19 +1,28 @@
 package com.example.esgithub.ui.fragments
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.esgithub.R
 import com.example.esgithub.ui.viewModels.ProfileViewModel
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : Fragment() {
     private val profileViewModel: ProfileViewModel by viewModel()
 
     private lateinit var userNameTv: TextView
+    private lateinit var userImage: ImageView
+
+    private val imageBaseUrl: String = "http://"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +35,9 @@ class ProfileFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        userNameTv = rootView.findViewById(R.id.userNameTextView)
+        userNameTv = rootView.findViewById(R.id.tv_name)
+        userImage = rootView.findViewById(R.id.profileImageId)
+
 
         setCurrentUserData()
 
@@ -35,7 +46,21 @@ class ProfileFragment : Fragment() {
 
     private fun setCurrentUserData() {
         profileViewModel.currentUserData.observe(viewLifecycleOwner) {
-            userNameTv.text = it.firstName
+            userNameTv.text = it.firstName + it.lastName
+
+            it.avatarUrl?.let { it1 -> setUserAvatar(it1) }
         }
+
+    }
+
+    private fun setUserAvatar(avatarLink: String) {
+        Picasso.get()
+            .load("$imageBaseUrl/$avatarLink")
+            .into(userImage, object : Callback {
+                override fun onSuccess() {}
+                override fun onError(e: Exception?) {
+                    Log.e("Picasso", "Error loading image: ${e?.message}")
+                }
+            })
     }
 }
